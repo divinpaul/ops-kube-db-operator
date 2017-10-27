d2804c69a8be88de8c8d4465fdb77c91bb5f37b9
Gustavo Hoirisch <guga@maurorisch.com.br>
2017-10-27 16:24:44 +1100

    The all present hack script! (#8)
    * gen client script
    
    * generate and delete backups in place

---
+++ hack/update-gen.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO=github.com/MYOB-Technology/ops-kube-db-operator
+HEADER_FILE="${GOPATH}/src/${REPO}/hack/boilerplate.go.txt"
+
+pushd ${GOPATH}/src/k8s.io/code-generator
+
+./generate-groups.sh all \
+    ${REPO}/pkg/client \
+    ${REPO}/pkg/apis "postgresdb:v1alpha1" \
+    --go-header-file ${GOPATH}/src/${REPO}/hack/boilerplate.go.txt
+
+popd
+
+find pkg -type f -exec grep -l myob-technology {} \;|xargs sed -i '.backup' -e 's/myob-technology/MYOB-Technology/g'
+
+find pkg/client/clientset/ -name *.backup -exec rm {} \;
